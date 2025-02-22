3de950bb6a72baf8cf786128f59c9c51bbad224d
anushka318 <anchaudhary@ucsd.edu>
2025-02-21 22:31:28 -0800

    Create data_cleaning.py
    Adding data_cleaning.py

---
+++ module/data_cleaning.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,Ridge,LogisticRegression
+from sklearn.metrics import r2_score , mean_squared_error ,confusion_matrix,accuracy_score
+import string as st
+from sklearn.preprocessing import LabelEncoder
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In[9]:
+
+
+pd.set_option('display.max_columns',500)
+pd.set_option('display.max_rows',200)
+df = pd.read_csv("C:/Program Files/R Files/anxiety.csv", encoding = 'ISO-8859-1')
+
+
+# In[10]:
+
+
+df.GAD2
+
+
+# In[11]:
+
+
+df.drop(['S. No.' , 'Timestamp'] , axis = 1 , inplace = True)
+
+
+# In[12]:
+
+
+df.shape
+
+
+# In[13]:
+
+
+df['Hours_streams'] = df['Hours'] + df['streams']
+df.drop(  ((df[df['Hours_streams'] > 115].index) | (df[df['Hours_streams']==0].index)),
+                                             axis=0,inplace=True)
+df.GADE.value_counts()
+
+
+# In[14]:
+
+
+df.GADE.fillna(df.GADE.value_counts().index[1] , inplace=True) #1
+df.GADE.value_counts()
+
+
+# In[15]:
+
+
+df.streams.fillna(int(df.streams.mean()) , inplace = True)
+df.Hours.fillna(int(df.Hours.mean()) , inplace = True)
+df.drop('Hours_streams' , axis = 1 , inplace = True)
+print(df.League.nunique())
+df.League = df.League.str.lower().str.strip()
+print(df.League.nunique())
+
+
+# In[16]:
+
+
+df["League"] =df["League"].str.extract(r'^([a-z]+)')
+df.League.nunique()
+
+
+# In[17]:
+
+
+df.League.unique()
+
+
+# In[18]:
+
+
+df.loc[(df['whyplay']== 'having fun') ,'League'] =df.loc[(df['whyplay']== 'having fun') ,'League'].fillna('unranked')
+df.League.fillna('gold' , inplace = True)
+golds = ['g', 'gv', 'golden' ,'glod' ,'golld' ,'golf', 'goled', 'golderino' ,'giii']
+df['League'] = df.League.replace(golds , 'gold')
+silvers = [ 'silverii' , 's' , 'sliver' , 'siver' , 'silber' , 'sil' , 'silveriv']
+df['League'] = df.League.replace(silvers , 'silver')
+plats = [
+    'platinium' , 'platnium' , 'platin' ,'pplatinum' ,'plarinum' ,'platium', 'p' ,'platine' ,
+    'platinun' ,'platonum' ,'platnum', 'plata' ,'plantinum',
+    'platinuim' ,'platunum', 'plantinum' ,'platunum' ,'platinumm' ,'platv' ,'platina' , 'plat' 
+]
+df['League'] = df.League.replace(plats , 'platinum')
+bronzers = ['bronce' , 'b' , 'broze' ,'lowest' , 'wood', 'elohell'] 
+df['League'] = df.League.replace(bronzers , 'bronze')
+unranked = ['none' ,'na', 'not' ,'n' ,'promos' ,'provisional' ,'placements' , 'dont' , 'was', 'unraked',
+            'havent', 'never', 'nope', 'no', 'noone', 'don', 'of', 'unrranked', 'new', 'what', 'unrank' ,
+            'ranked', 'placement', 'unrankt' , 'non', 'unfranked' , 'promotion', 'idk',
+            'unplaced', 'probably', 'provisionals', 'didnt' ,'unrakned' , 'unfinished' , 'just' , 'x' ,
+            'promotions' , 'unseeded' , 'haven']
+df['League'] = df.League.replace(unranked , 'unranked')
+diamonds =  ['d', 'dia', 'diaomnd', 'diamont','diamomd']
+df['League'] = df.League.replace(diamonds , 'diamond')
+gm =  ['grand', 'gm', 'grandmasters']
+df['League'] = df.League.replace(gm , 'grandmaster')
+chall =  ['challenjour', 'c', 'charrenjour', 'challeneger']
+df['League'] = df.League.replace(chall , 'challenger')
+df['League'] = df.League.replace('mg' , 'mge')
+df['League'] = df.League.replace('masters' , 'master')
+df['League'] = df.League.replace( ['le', 'legdendary'] , 'legendary')
+df.League.value_counts()
+
+
+# In[19]:
+
+
+counts = df['League'].value_counts()
+df['League'] = df['League'][~df['League'].isin(counts[counts < 3].index)]
+df['League'] = df.League.replace(['i' , 'currently' , 'high' , 'season' , 'lol','cs' ,
+                                  'last' ,'csgo','starcraft' ,'geater' , 'in', 'rank' , 'still'] , np.nan)
+df.League.fillna('unspecified' , inplace=True)
+df.League.unique()
+
+
+# In[20]:
+
+
+df['Narcissism'].value_counts()
+
+
+# In[21]:
+
+
+df.drop(["Birthplace","Birthplace_ISO3"],axis=1,inplace=True)
+df['Residence'] = df['Residence'].replace('Unknown',df['Residence'].mode()[0])
+df['Reference'].fillna('Other',inplace=True)
+df.drop(df[df['accept'].isnull()].index , axis=0 , inplace=True)
+df['Residence_ISO3'].fillna('USA',inplace=True) #11063
+df.loc[11063,'Residence_ISO3'] = 'XXK'
+col = ['SPIN1','SPIN2','SPIN3','SPIN4','SPIN5','SPIN6','SPIN7','SPIN8','SPIN9',
+     'SPIN10','SPIN11','SPIN12','SPIN13','SPIN14','SPIN15','SPIN16','SPIN17' ,'SPIN_T']
+for i in col :
+    df[i].fillna(df[i].mode()[0], inplace = True)
+df['Playstyle'] = df['Playstyle'].apply(lambda x: ' '.join(word.strip(st.punctuation) for word in x.split()))
+df['earnings'] = df['earnings'].apply(lambda x: ' '.join(word.strip(st.punctuation) for word in x.split()))
+df['whyplay'] = df['whyplay'].apply(lambda x: ' '.join(word.strip(st.punctuation) for word in x.split()))
+df['Playstyle'] = df['Playstyle'].str.lower().str.strip()
+df['whyplay'] = df['whyplay'].str.lower().str.strip()
+df['earnings'] = df['earnings'].str.lower().str.strip()
+df['Playstyle'].nunique()
+
+
+# In[22]:
+
+
+df.drop('highestleague' , axis = 1 , inplace = True)
+df.isnull().sum()
+
+
+# In[23]:
+
+
+df.Work.fillna(df.Work.mode()[0] , inplace=True)
+df.drop(['Residence' , 'accept'] , axis = 1 , inplace = True)
+df.dtypes
+
+
+# In[24]:
+
+
+df.earnings.replace(df.earnings.value_counts().index[3:] , 'Other',inplace=True)
+df['earnings'].value_counts()
+
+
+# In[25]:
+
+
+df.whyplay.replace(df.whyplay.value_counts().index[5:] , 'Other',inplace=True)
+df['whyplay'].value_counts()
+
+
+# In[26]:
+
+
+df.Playstyle.replace(df.Playstyle.value_counts().index[5:] , 'Other',inplace=True)
+df['Playstyle'].value_counts()
+
+
+# In[27]:
+
+
+df.Playstyle.replace('Other' , np.nan , inplace=True)
+df.whyplay.replace('Other' , np.nan , inplace=True)
+df.earnings.replace('Other' , np.nan , inplace=True)
+df.dropna(inplace=True)
+df.shape
